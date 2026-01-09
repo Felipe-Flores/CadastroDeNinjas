fedc8aacce8b908597ba1efdb8423dced1dea868
Felipe-Flores <felipe.floresceo@gmail.com>
2026-01-09 16:54:07 -0400

    feat: Insere o Lombok na classe para abstracao

--- src/main/java/com/fftech/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/com/fftech/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,18 @@ package com.fftech.CadastroDeNinjas.Missoes;
 
 import com.fftech.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class MissoesModel {
 
     @Id
